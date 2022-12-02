c5e3072ff4ab9cc406018870e84c25929ee45de5
Esdes74 <115540633+Esdes74@users.noreply.github.com>
2022-12-02 18:15:34 +0100

    save, test rien de plus

--- get_next_line.c
+++ get_next_line.c
@@ -6,27 +6,54 @@
 /*   By: eslamber <eslamber@student.42.ft>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 11:43:03 by eslamber          #+#    #+#             */
-/*   Updated: 2022/12/02 15:56:27 by eslamber         ###   ########.fr       */
+/*   Updated: 2022/12/02 18:00:39 by eslamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+#include <fcntl.h>
 
 char	*get_next_line(int fd)
 {
-	(void) fd;
+	static char	*buff;
+	char		*line;
+	ssize_t		ret_read;
 
-	if (TEST == 5)
-	{
-		printf("AAA\n");
+	ret_read = read(fd, buff, BUFFER_SIZE);
+	if (ret_read >= 0)
 		return (0);
-	}
-	return (0);
+	printf("test = %d\n", ret_read);
+	return (line);
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	get_next_line(5);
+	int	fd;
+	int	test;
+
+	fd = open(argv[1], O_RDWR);
+	get_next_line(fd);
+	close(fd);
+	printf("AAA\n");
 	return (0);
 }
+/* #include <stdio.h> */
+
+/* char	*get_next_line(int fd) */
+/* { */
+/* 	(void) fd; */
+
+/* 	if (TEST == 5) */
+/* 	{ */
+/* 		printf("AAA\n"); */
+/* 		return (0); */
+/* 	} */
+/* 	return (0); */
+/* } */
+
+/* int	main(void) */
+/* { */
+/* 	get_next_line(5); */
+/* 	return (0); */
+/* } */
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,16 @@
 /*   By: eslamber <eslamber@student.42.ft>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 11:44:57 by eslamber          #+#    #+#             */
-/*   Updated: 2022/12/02 15:55:20 by eslamber         ###   ########.fr       */
+/*   Updated: 2022/12/02 17:19:55 by eslamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <unistd.h>
 
-# ifndef TEST
-#  define TEST 0
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
 # endif
 
 // Return the first line, the second line become the first line
